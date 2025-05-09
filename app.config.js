ee23e37add64e5b24ffc940333a562699506c462
Kdorveus <liljcool45@gmail.com>
2025-05-09 18:16:01 -0400

    Increment Android version code to 2

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ export default {
         "backgroundColor": "#ffffff"
       },
       "package": "com.calculator.calcai",
-      "versionCode": 1,
+      "versionCode": 2,
       "allowBackup": false,
       "permissions": [
         "RECORD_AUDIO",
